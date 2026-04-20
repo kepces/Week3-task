3c86e48670e5679a68d2c1b8c964678e5da02aea
kepces <ekchhantarai+accounts@outlook.com>
2026-04-20 10:13:33 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -5,24 +5,26 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
-DigitalIn enterButton(BUTTON1);
-DigitalIn gasDetector(D2);
-DigitalIn overTempDetector(D3);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
+//DigitalIn enterButton(BUTTON1);
+DigitalIn toggleGasAlarmS1(D2);
+DigitalIn requestGasStateS2(D3);
+DigitalIn requestTemperatureAlarmS3(D4);
+DigitalIn toggleTemperatureAlarmS4(D5);
+DigitalIn resetAlarmsS5(D6);
+DigitalIn toggleContinousModeS6(D7);
 
-DigitalOut alarmLed(LED1);
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
+DigitalOut gasStateLed(LED1);
+DigitalOut temperatureStateLed(LED2);
+DigitalOut continousModeLed(LED3);
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 //=====[Declaration and initialization of public global variables]=============
 
-bool alarmState = OFF;
-int numberOfIncorrectCodes = 0;
+bool DeleteThis = OFF;
+bool gasAlarm = OFF;
+bool overTemperatureAlarm = OFF;
+bool continousModeEnabled = OFF;
 
 //=====[Declarations (prototypes) of public functions]=========================
 
@@ -30,10 +32,13 @@ void inputsInit();
 void outputsInit();
 
 void alarmActivationUpdate();
-void alarmDeactivationUpdate();
 
-void uartTask();
+void sendToUart();
 void availableCommands();
+
+void readButtons();
+
+void continousOutputTask();
 
 //=====[Main function, the program entry point after power on or reset]========
 
@@ -42,9 +47,9 @@ int main()
     inputsInit();
     outputsInit();
     while (true) {
-        alarmActivationUpdate();
-        alarmDeactivationUpdate();
-        uartTask();
+        readButtons();
+        continousOutputTask();
+        delay(200);
     }
 }
 
@@ -52,68 +57,114 @@ int main()
 
 void inputsInit()
 {
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
+    toggleGasAlarmS1.mode(PullDown);
+    requestGasStateS2.mode(PullDown);
+    requestTemperatureAlarmS3.mode(PullDown);
+    toggleTemperatureAlarmS4.mode(PullDown);
+    resetAlarmsS5.mode(PullDown);
+    toggleContinousModeS6.mode(PullDown);
 }
 
 void outputsInit()
 {
-    alarmLed = OFF;
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
+    gasStateLed = OFF;
+    temperatureStateLed = OFF;
+    continousModeLed = OFF;
 }
 
-void alarmActivationUpdate()
+void readButtons()
 {
-    if ( gasDetector || overTempDetector ) {
-        alarmState = ON;
-    }
-    alarmLed = alarmState;
-}
+    if ( toggleGasAlarmS1 ) {
+        gasAlarm = !gasAlarm;
+        gasStateLed = !gasStateLed;
 
-void alarmDeactivationUpdate()
-{
-    if ( numberOfIncorrectCodes < 5 ) {
-        if ( aButton && bButton && cButton && dButton && !enterButton ) {
-            incorrectCodeLed = OFF;
-        }
-        if ( enterButton && !incorrectCodeLed && alarmState ) {
-            if ( aButton && bButton && !cButton && !dButton ) {
-                alarmState = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
-            }
-        }
-    } else {
-        systemBlockedLed = ON;
-    }
-}
-
-void uartTask()
-{
-    char receivedChar = '\0';
-    if( uartUsb.readable() ) {
-        uartUsb.read( &receivedChar, 1 );
-        if ( receivedChar == '1') {
-            if ( alarmState ) {
-                uartUsb.write( "The alarm is activated\r\n", 24);
-            } else {
-                uartUsb.write( "The alarm is not activated\r\n", 28);
-            }
+        if ( gasAlarm ) {
+            uartUsb.write("WARNING: GAS DETECTED\r\n", 23);
         } else {
-            availableCommands();
+            uartUsb.write("GAS SET TO NOT DETECTED\r\n", 25);
+        } 
+        delay(200);
+    }
+
+    if ( requestGasStateS2 ) {
+        if ( gasAlarm ) {
+            uartUsb.write("GAS ALARM ACTIVE\r\n", 20);
+        } else {
+            uartUsb.write("GAS ALARM CLEAR\r\n", 19);
         }
+    }
+
+    if ( requestTemperatureAlarmS3 ) {
+        if ( overTemperatureAlarm ) {
+            uartUsb.write("TEMP ALARM ACTIVE\r\n", 21);
+        } else {
+            uartUsb.write("TEMP ALARM CLEAR\r\n", 20);
+        }
+    }
+
+    if ( toggleTemperatureAlarmS4 ) {
+        overTemperatureAlarm = !overTemperatureAlarm;
+        temperatureStateLed = !temperatureStateLed;
+
+        if ( overTemperatureAlarm ) {
+            uartUsb.write("WARNING: TEMPERATURE TOO HIGH\r\n", 31);
+        } else {
+            uartUsb.write("TEMPERATURE SET TO NOT HIGH\r\n", 29);
+        } 
+        delay(200);
+    }
+    
+    if ( resetAlarmsS5 ) {
+        gasAlarm = gasStateLed = overTemperatureAlarm = temperatureStateLed = OFF;
+        uartUsb.write("ALARMS RESET\r\n", 14);
+    }
+
+    if ( toggleContinousModeS6 ) {
+        continousModeEnabled = !continousModeEnabled;
+
+        if ( continousModeEnabled ) {
+            uartUsb.write("Continous mode enabled\r\n", 24);
+        }
+    }
+    delay(2000);
+}
+
+void continousOutputTask()
+{
+    if ( continousModeEnabled ) {
+        int buffer_size = 23;  // length of string without the on or off letters
+        const char* gasStateString;
+        const char* temperatureStateString;
+
+        if ( gasAlarm )  {
+            gasStateString = "ON";
+            buffer_size += 2;
+        } else {
+            gasStateString = "OFF";
+            buffer_size += 3;
+        }
+        if ( overTemperatureAlarm )  {
+            temperatureStateString = "ON";
+            buffer_size += 2;
+        } else {
+            temperatureStateString = "OFF";
+            buffer_size += 3;
+        }
+
+        char buffer[buffer_size];
+        sprintf(buffer, "GAS: %s\t Temperature: %s\r\n", gasStateString, temperatureStateString);
+        uartUsb.write(buffer, buffer_size);
+        delay(2000);
     }
 }
 
 void availableCommands()
 {
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '1' to get the alarm state\r\n\r\n", 36 );
+    uartUsb.write( "Available commands:\r\n", 23 );
+    uartUsb.write( "Press '1' to toggle the gas alarm state\r\n", 43 );
+    uartUsb.write( "Press '2' to get the gas detector state\r\n", 43 );
+    uartUsb.write( "Press '3' to get the over temperature detector state\r\n", 56 );
+    uartUsb.write( "Press '4' to toggle the over-temperature\r\n", 44 );
+    uartUsb.write( "Press '5' to reset the both alarms\r\n\r\n", 42 );
+    uartUsb.write( "Press '6' enter or exit continous monitoring mode\r\n\r\n", 57 );
 }
